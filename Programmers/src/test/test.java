67f4ad66118843f6dff83eb6c86f3f34cc1f080a
NELEF\imone <imoneleft@naver.com>
2023-04-29 10:14:02 +0900

    programmers 폴더 생성

---
+++ Programmers/src/test/test.java
@@ -0,0 +1,11 @@
+package test;
+
+public class test {
+	public static void main(String[] args) {
+		int[] arr = new int[3];
+
+		for (int i = 0; i < 3; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
